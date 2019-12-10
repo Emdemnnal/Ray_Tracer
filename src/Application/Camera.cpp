f97dcec4b439181d4e52d92688db200991720dbe
Crisolle Infante (s4919313) <s4919313@bournemouth.ac.uk>
2019-12-10 15:39:00 +0000

    Trying to finish Camera Class.

--- src/Application/Camera.cpp
+++ src/Application/Camera.cpp
@@ -14,12 +14,22 @@ Ray Camera::createRay(glm::ivec2 position)
 Ray Camera::createRay(glm::ivec2 position)
 {
 	// Pixel Co-ordinates to map to NDC (Normalised Device Co-ordinates).
-	glm::vec2 pixelCoords = glm::vec2(0, 0);
+	//glm::vec2 pixelCoords = glm::vec2(0, 0);
 
+	// Initialize Near and Far Plane.
+	glm::vec4 nearPlane;
+	glm::vec4 farPlane;
+	
 	// Near Plane (z = -1).
-	glm::vec4 nearPlane = glm:vec4(pixelCoords, -1, 1);
+	nearPlane.x = ((2.0 * position.x) - (2.0 * 0) / 640) - 1.0);
+	nearPlane.y = ((2.0 * position.y) - (2.0 * 0) / 480) - 1.0);
+	nearPlane.z = -1.0;
+	nearPlane.w = 1.0;
 	// Far Plane (z = 1).
-	glm::vec4 farPlane = glm::vec4(pixelCoords, 1, 1);
+	farPlane.x = ((2.0 * position.x) - (2.0 * 0) / 640) - 1.0);
+	farPlane.y = ((2.0 * position.y) - (2.0 * 0) / 480) - 1.0);
+	farPlane.z = 1.0;
+	farPlane.w = 1.0;
 
 	// Inverse Projection Matrix.
 	invProjMat = glm::inverse(projMat);
@@ -41,7 +51,7 @@ Ray Camera::createRay(glm::ivec2 position)
 	// Returned ray.
 	Ray ray;
 	ray.origin = glm::vec3(nearPlane);
-	ray.direction = glm::vec3(0, 0, -1);
+	ray.direction = glm::normalize(glm::vec3(farPlane) - nearPlane);
 
 	return ray;
 };
--- src/Application/Camera.h
+++ src/Application/Camera.h
@@ -6,6 +6,9 @@ class Ray;
 class Camera
 {
 public:
+  Camera();
+  ~Camera();
+
   Ray createRay(glm::ivec2 position);
 protected:
   glm::mat4 viewMat, projMat, invViewMat, invProjMat;
