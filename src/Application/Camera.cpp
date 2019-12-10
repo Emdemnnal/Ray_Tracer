5e081a412020e59d23037a731c62447dc7f9db7b
Crisolle Infante (s4919313) <s4919313@bournemouth.ac.uk>
2019-12-10 15:57:13 +0000

    Finishing Camera Errors.

--- src/Application/Camera.cpp
+++ src/Application/Camera.cpp
@@ -21,13 +21,13 @@ Ray Camera::createRay(glm::ivec2 position)
 	glm::vec4 farPlane;
 	
 	// Near Plane (z = -1).
-	nearPlane.x = ((2.0 * position.x) - (2.0 * 0) / 640) - 1.0);
-	nearPlane.y = ((2.0 * position.y) - (2.0 * 0) / 480) - 1.0);
+	nearPlane.x = ((2.0 * position.x) - (2.0 * 0) / 640.0) - 1.0;
+	nearPlane.y = ((2.0 * position.y) - (2.0 * 0) / 480.0) - 1.0;
 	nearPlane.z = -1.0;
 	nearPlane.w = 1.0;
 	// Far Plane (z = 1).
-	farPlane.x = ((2.0 * position.x) - (2.0 * 0) / 640) - 1.0);
-	farPlane.y = ((2.0 * position.y) - (2.0 * 0) / 480) - 1.0);
+	farPlane.x = ((2.0 * position.x) - (2.0 * 0) / 640.0) - 1.0;
+	farPlane.y = ((2.0 * position.y) - (2.0 * 0) / 480.0) - 1.0;
 	farPlane.z = 1.0;
 	farPlane.w = 1.0;
 
@@ -50,8 +50,8 @@ Ray Camera::createRay(glm::ivec2 position)
 
 	// Returned ray.
 	Ray ray;
-	ray.origin = glm::vec3(nearPlane);
-	ray.direction = glm::normalize(glm::vec3(farPlane) - nearPlane);
+	ray.origin = glm::vec4(nearPlane);
+	ray.direction = glm::normalize(glm::vec4(farPlane) - nearPlane);
 
 	return ray;
 };
